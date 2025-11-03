c246aae5ad784e9a95219f0ecd54992b911f7e7f
tjddn327 <sungwk100@gmail.com>
2025-11-03 13:36:22 +0900

    entity1

--- src/main/java/com/nhnacademy/springbootjpa/entity/Item.java
+++ src/main/java/com/nhnacademy/springbootjpa/entity/Item.java
@@ -1,5 +1,10 @@
 package com.nhnacademy.springbootjpa.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
 // TODO #1: `item` 테이블과 매핑될 `Item` Entity 클래스를 작성하세요.
 /*
  * create table item
@@ -10,16 +15,25 @@ package com.nhnacademy.springbootjpa.entity;
  *     price bigint      not null
  * );
  */
+@Entity
+@Table(name = "item")
 public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Getter
+    @NotNull
+    @Column(length = 40)
+    private String name;
+
+    @Getter
+    @NotNull
+    private long price;
+
+
     public long getId() {
-        return 0;
+        return this.id;
     }
 
-    public String getName() {
-        return null;
-    }
-
-    public long getPrice() {
-        return 0;
-    }
 }
