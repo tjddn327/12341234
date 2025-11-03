e58478610bdc6837ea2c72620a47b7da07ed4cec
tjddn327 <sungwk100@gmail.com>
2025-11-03 13:29:28 +0900

    123

--- src/main/java/com/nhnacademy/springbootjpa/repository/UserRepository.java
+++ src/main/java/com/nhnacademy/springbootjpa/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.nhnacademy.springbootjpa.repository;
 
 import com.nhnacademy.springbootjpa.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface UserRepository extends JpaRepository<User, String> {
 }
